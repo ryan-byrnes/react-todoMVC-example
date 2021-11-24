3d41d19a23a56ab5dd5b5513d0875b54462b9df6
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-11-24 15:29:54 -0800

    completed test 6

--- react/cypress/integration/sold-tests/tasks-management.spec.js
+++ react/cypress/integration/sold-tests/tasks-management.spec.js
@@ -7,36 +7,48 @@ describe('Tasks Management', () => {
     cy.visit('/');
   });
 
-  it('checks all todos and has complete class', () => {
-    cy.setUp();
-    cy.get('input.toggle').click({ multiple: true });
-    cy.get('li').should('have.class', 'completed');
-  })
+  // it('checks all todos and has complete class', () => {
+  //   cy.setUp();
+  //   cy.get('input.toggle').click({ multiple: true });
+  //   cy.get('li').should('have.class', 'completed');
+  // })
 
-  it('toggles complete to remove completed class', () => {
-    cy.setUp();
-    cy.get('input.toggle').click({ multiple: true }).click({ multiple: true });
-    cy.get('li').should('not.have.class', 'completed');
-  })
+  // it('toggles complete to remove completed class', () => {
+  //   cy.setUp();
+  //   cy.get('input.toggle').click({ multiple: true }).click({ multiple: true });
+  //   cy.get('li').should('not.have.class', 'completed');
+  // })
 
-  it('.todo-count has text 2 items left', () => {
-    cy.setUp();
-    cy.get('input.toggle').first().click();
-    cy.get('.todo-count').contains('2 items left');
-  })
+  // it('.todo-count has text 2 items left', () => {
+  //   cy.setUp();
+  //   cy.get('input.toggle').first().click();
+  //   cy.get('.todo-count').contains('2 items left');
+  // })
 
-  it('Clear Completed is available after a todo is completed', () => {
-    cy.setUp();
-    cy.get('input.toggle').first().click();
-    cy.get('button.clear-completed').should('be.visible');
-  })
+  // it('Clear Completed is available after a todo is completed', () => {
+  //   cy.setUp();
+  //   cy.get('input.toggle').first().click();
+  //   cy.get('button.clear-completed').should('be.visible');
+  // })
 
-  it('completed todo no longer exists after clicking Clear Completed', () => {
-    cy.setUp();
-    const firstInput = cy.get('input.toggle').first();
-    firstInput.click();
-    cy.get('button.clear-completed').click();
-    firstInput.should('not.exist');
+  // it('completed todo no longer exists after clicking Clear Completed', () => {
+  //   cy.setUp();
+  //   const firstInput = cy.get('input.toggle').first();
+  //   firstInput.click();
+  //   cy.get('button.clear-completed').click();
+  //   firstInput.should('not.exist');
+  // })
+
+  // it('controls hidden when editing', () => {
+  //   cy.setUp();
+  //   cy.get('li').first().dblclick();
+  //   cy.get('li.editing > input.toggle').should('not.exist');
+  //   cy.get('button.destroy').should('not.be.visible');
+
+  // })
+
+  it('highlight currently applied filter', () => {
+
   })
 
 
